97dfbf1a8ac6e21e4667372327e19d8679143c45
Muhindo-Galien <muhindogalien512@gmail.com>
2023-01-20 04:10:06 +0300

    added header and Barnner

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Banner from "./components/Banner"
+import NavBar from "./components/NavBar"
+
+
+const App = () => {
+  return (
+    <div className="">
+      <NavBar/>
+      <Banner/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import gblockcahin from "../assets/gblockchain.png"
+
+const NavBar = () => {
+  const connectedAccount = true
+  return (
+    <div className=" sm:px-8 bg-[#000] z-50 mx-auto w-full fixed shadow-lg">
+        <div className=' flex items-center justify-between py-4 sm:mx-0 mx-4 '>
+          <Link to={'/'}>
+            <img src={gblockcahin} width={50} height={50} alt="logo" className='rounded'/> 
+          </Link>
+            <div className='flex gap-4'>
+              <div className="rounded-full shadow-md">
+              </div>
+              {connectedAccount?
+               ( <button disabled type='button' className='bg-[#fd1c1c]  px-3 py-2 rounded text-gray-100 font-semibold'>
+                    {/* {truncate(connectedAccount,6,6,15)} */} ox7dfdu..3s9uys
+                </button> ):(
+                  // onClick={()=>connectWallet()}
+                  <button type='button' className='bg-[#fd1c1c] font-medium  px-3 py-2 rounded text-gray-50' >
+                  Connect Wallet
+                </button> 
+                )
+              }
+
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default NavBar
